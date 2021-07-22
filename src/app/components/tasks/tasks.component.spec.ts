47c3e338af28f65dfd94cb9b8ca2703603686b2b
cynthia chebet <chebetcynthia8@gmail.com>
2021-07-22 09:36:20 +0300

    added HttpClientTestingModule

--- src/app/components/tasks/tasks.component.spec.ts
+++ src/app/components/tasks/tasks.component.spec.ts
@@ -1,3 +1,5 @@
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TasksComponent } from './tasks.component';
@@ -8,7 +10,8 @@ describe('TasksComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TasksComponent ]
+      declarations: [ TasksComponent ],
+      imports: [HttpClientTestingModule], 
     })
     .compileComponents();
   });
